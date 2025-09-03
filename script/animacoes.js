943c588ef440dd1c14f7d38458c411a1ded9a1b3
webdevdesiner <fogsgop.revendas@gmail.com>
2025-09-03 14:41:24 -0300

    mition banner top

---
+++ script/animacoes.js
@@ -0,0 +1,20 @@
+  // Pulso infinito
+  gsap.to("#pulsoInfinito", {
+    duration: 1.5,
+    scale: 1.05,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+  });
+
+
+  // Pulso infinito mais lento
+  gsap.to("#pulsoInfinitolento", {
+    duration: 3.0,
+    scale: 1.05,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+  });
+
+
